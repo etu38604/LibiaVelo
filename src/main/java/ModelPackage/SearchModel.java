efe00bf0d0d8ca3131a79dcbeaa5d2b23f399578
Damien <damien.fricot@hotmail.be>
2018-08-17 20:58:44 +0200

    searchDataAccess + SearchListingPanel + searchModel + insert1Panel (facultatif) + windowClosing +listingPanel (inCharge) + front DeletePanel

---
+++ src/main/java/ModelPackage/SearchModel.java
@@ -0,0 +1,10 @@
+package DataAccessPackage;
+
+import java.util.GregorianCalendar;
+
+public class SearchModel {
+
+    private GregorianCalendar dateTransport;
+    private Integer idVelo;
+    private 
+}
---
+++ src/main/java/ViewPackage/SearchListingPanel.java
@@ -0,0 +1,4 @@
+package ViewPackage;
+
+public class SearchListingPanel {
+}
